7a0e199b3a0ecdd2e37242a472082d1282359a1c
Tracy Ngo <tngo1206@gmail.com>
2019-03-31 02:05:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,6 +27,7 @@ attr_accessor :name
     self.class.all << self 
   end
   
+<<<<<<< HEAD
   def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
     if found_artist
@@ -36,6 +37,18 @@ attr_accessor :name
       new_artist.save
       new_artist
     end
+=======
+  def self.find_or_create_by_name(name)
+    
+     if self.all.find { |artist| artist.name == name} 
+      self.name
+     else 
+       artist = self.new(name)
+       artist.save
+       artist
+      end
+    
+>>>>>>> 83b5cfe3a84ea6b092a58088ba079596d3a1e70a
   end
   
   def print_songs
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 class Song
@@ -20,4 +21,24 @@ class Song
       artist.add_song(self)
   end
   
+=======
+require 'artist.rb'
+
+class Song
+  
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.new_by_filename(filename)
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
+  
+  
+>>>>>>> 83b5cfe3a84ea6b092a58088ba079596d3a1e70a
 end
